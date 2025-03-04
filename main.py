e8c23bf9e12165422b99b8d88735bb548b81b36d
Scott Hallock <scott@hallocks.net>
2025-03-04 11:33:12 -0700

    Implement Sidewinder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from maze.maze import Maze
-from maze_algorithm.binary_tree import binary_tree
+from maze_algorithm.sidewinder import sidewinder
 from maze_render.svg import svg_distance
 
 def main():
@@ -8,7 +8,7 @@ def main():
     cell_width = 20
     cell_height = 20
     maze = Maze(maze_width, maze_height)
-    binary_tree(maze)
+    sidewinder(maze)
     svg_distance(maze, cell_width, cell_height)
 
 if __name__ == "__main__":
---
+++ maze_algorithm/sidewinder.py
@@ -0,0 +1,24 @@
+import random
+
+def close_run(maze, run):
+    coordinate = random.choice(run)
+    neighbors = maze.neighbors[maze.linearize(coordinate)]
+    if 'north' in neighbors:
+        maze.link(coordinate, neighbors['north'])
+    run.clear()
+
+def sidewinder(maze):
+    random.seed()
+    run = []
+    for coordinate in [(x, y) for y in range(maze.height) for x in range(maze.width)]:
+        run.append(coordinate)
+        neighbors = maze.neighbors[maze.linearize(coordinate)]
+        if 'north' in neighbors and 'east' in neighbors:
+            if random.random() > 0.5:
+                maze.link(coordinate, neighbors['east'])
+            else:
+                close_run(maze, run)
+        elif 'north' in neighbors:
+            close_run(maze, run)
+        elif 'east' in neighbors:
+            maze.link(coordinate, neighbors['east'])
