78bf6d4d2575535e7123580265058a9741ee312a
Scott Hallock <scott@hallocks.net>
2025-03-04 11:45:49 -0700

    Correct formatting

--- maze_algorithm/sidewinder.py
+++ maze_algorithm/sidewinder.py
@@ -1,5 +1,6 @@
 import random
 
+
 def close_run(maze, run):
     coordinate = random.choice(run)
     neighbors = maze.neighbors[maze.linearize(coordinate)]
@@ -7,10 +8,12 @@ def close_run(maze, run):
         maze.link(coordinate, neighbors['north'])
     run.clear()
 
+
 def sidewinder(maze):
     random.seed()
     run = []
-    for coordinate in [(x, y) for y in range(maze.height) for x in range(maze.width)]:
+    for coordinate in [(x, y) for y in range(maze.height)
+                       for x in range(maze.width)]:
         run.append(coordinate)
         neighbors = maze.neighbors[maze.linearize(coordinate)]
         if 'north' in neighbors and 'east' in neighbors:
